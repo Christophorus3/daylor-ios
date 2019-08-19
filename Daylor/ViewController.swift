a9fe8d982c188435f60e389586efc51c5c2fdadc
christophwottawa <christoph.wottawa@me.com>
2019-08-19 20:32:02 +0200

    Initial Commit

---
+++ Daylor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daylor
+//
+//  Created by Christoph Wottawa on 19.08.19.
+//  Copyright © 2019 Christoph Wottawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
